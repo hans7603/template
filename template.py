7d5fdd7ecd214cee8148bc64e9467e6edc807517
hans7603 <51442734+hans7603@users.noreply.github.com>
2019-06-09 19:53:54 +0200

    Update template.py

--- template.py
+++ template.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hermes_python.hermes import Hermes
+
+
+def action_wrapper(hermes, intent_message):
+    result_sentence = "Hallo, ist da jemand?"
+
+    current_session_id = intent_message.session_id
+    hermes.publish_end_session(current_session_id, result_sentence)
+
+
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+h.subscribe_intent("domi:getAddition", action_wrapper).start()
