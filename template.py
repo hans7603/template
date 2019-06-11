1c5433d045768b789c847eba26bb452707ef5d2b
hans7603 <51442734+hans7603@users.noreply.github.com>
2019-06-11 08:52:29 +0200

    Update template.py

--- template.py
+++ template.py
@@ -13,4 +13,4 @@ def action_wrapper(hermes, intent_message):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("domi:getAddition", action_wrapper).start()
+        h.subscribe_intent("kaiserdom:Temperatur", action_wrapper).start()
